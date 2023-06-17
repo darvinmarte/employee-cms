630d676c5f1a720844f4d15facb111326de46bd0
darvinmarte <119225399+darvinmarte@users.noreply.github.com>
2023-06-16 20:58:49 -0400

    added initial seed code

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department (id, name)
+VALUES (001, ""),
+       (002, ""),
+       (003, ""),
+       (004, "");
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (001, ""),
+       (002, ""),
+       (003, ""),
+       (004, "");
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (001, ""),
+       (002, ""),
+       (003, ""),
+       (004, "");
